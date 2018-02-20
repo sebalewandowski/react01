85be0f706686f2c5e143b6496f7ed232f0134b46
Jan Szłapka <j_szlapka@interia.pl>
2018-02-20 05:04:09 +0100

    infinite-scroll with data load

--- my-app/src/components/GridComponent.js
+++ my-app/src/components/GridComponent.js
@@ -8,7 +8,7 @@ import Price from './Price';
 
   function GridComponent(props) {
     return (
-     <div className='col-sm-6 col-md-4 col-lg-2 rounded'>
+     <div className='col-sm-6 col-md-4 col-lg-2 rounded float-left'>
           <ImageComponent size={props.size} src={props.src}/>
           <Size size={props.size}/>
           <Price price={props.price}/>
